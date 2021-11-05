c3290dd9800199e36ecb4edcb28e82996a16f45e
Pchela <lyudmilalittlepchela@gmail.com>
2021-11-05 21:33:39 +0200

    ToDoItems property

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        loadData()
-        
         // Override point for customization after application launch.
         return true
     }
--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,37 +7,32 @@
 
 import Foundation
 
-var ToDoItems: [[String: Any]] = [["Name": "Позвонить маме", "isCompleted": false], ["Name": "Купить хлеб", "isCompleted": false], ["Name": "Покормить кота", "isCompleted": false]]
+var ToDoItems: [[String: Any]] {
+    
+    set{
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    
+    get{
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
+}
 
 func addItem(nameItem: String, isCompleted: Bool = false) {
     ToDoItems.append(["Name": nameItem, "isCompleted": false])
-    
-    saveData()
 }
 
 func removeItem(at index: Int) {
     ToDoItems.remove(at: index)
-    
-    saveData()
 }
 
 func changeState(at item: Int) -> Bool {
     ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
     
-    saveData()
-    
     return (ToDoItems[item]["isCompleted"] as! Bool)
-}
-
-func saveData() {
-    UserDefaults.standard.set(ToDoItems, forKey: "ToDoDataKey")
-    UserDefaults.standard.synchronize()
-}
-
-func loadData() {
-    if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]] {
-        ToDoItems = array
-    } else {
-        ToDoItems = []
-    }
 }
