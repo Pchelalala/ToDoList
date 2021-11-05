695f1b066516cec5dd169caf0f7f83598faec9c5
Pchela <lyudmilalittlepchela@gmail.com>
2021-11-05 20:18:04 +0200

    add and delete items

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+var ToDoItems: [String] = ["Позвонить маме", "Купить хлеб"]
+
+func addItem(nameItem: String) {
+    ToDoItems.append(nameItem)
+    saveData()
+}
+
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+    saveData()
+}
+
+func saveData() {
+    
+}
+
+func loadData() {
+    
+}
