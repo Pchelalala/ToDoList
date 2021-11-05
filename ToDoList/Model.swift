cff9c35d5eb53aa9b5d02ce04a2b22956445e414
Pchela <lyudmilalittlepchela@gmail.com>
2021-11-05 21:11:17 +0200

    checkmarks

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,16 +7,26 @@
 
 import Foundation
 
-var ToDoItems: [String] = ["Позвонить маме", "Купить хлеб"]
+var ToDoItems: [[String: Any]] = [["Name": "Позвонить маме", "isCompleted": false], ["Name": "Купить хлеб", "isCompleted": false], ["Name": "Покормить кота", "isCompleted": false]]
 
-func addItem(nameItem: String) {
-    ToDoItems.append(nameItem)
+func addItem(nameItem: String, isCompleted: Bool = false) {
+    ToDoItems.append(["Name": nameItem, "isCompleted": false])
+    
     saveData()
 }
 
 func removeItem(at index: Int) {
     ToDoItems.remove(at: index)
+    
     saveData()
+}
+
+func changeState(at item: Int) -> Bool {
+    ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
+    
+    saveData()
+    
+    return (ToDoItems[item]["isCompleted"] as! Bool)
 }
 
 func saveData() {
--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -40,8 +40,15 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = ToDoItems[indexPath.row]
-        // Configure the cell...
+        
+        let currentItem = ToDoItems[indexPath.row]
+        cell.textLabel?.text = currentItem["Name"] as? String
+        
+        if (currentItem["isCompleted"] as? Bool) == true {
+            cell.accessoryType = .checkmark
+        } else{
+            cell.accessoryType = .none
+        }
 
         return cell
     }
@@ -63,6 +70,23 @@ class TableViewController: UITableViewController {
         }    
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if changeState(at: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        // 2 ways of reloading:
+        //tableView.reloadData()
+        tableView.cellForRow(at: indexPath)?.accessoryType
+        
+        
+    }
+    
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
